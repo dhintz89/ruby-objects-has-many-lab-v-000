e8dcc1da5a71cae81af1d98f7c57c340e3a0a26e
Dan Hintz <dhintz@live.com>
2018-09-03 19:12:35 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,3 +11,16 @@ class Artist
   end
   
   def add_song(song)
+    @songs << song
+  end
+  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+    @songs << song
+  end
+  
+  def song_count
+    @song.length
+  end
+end
